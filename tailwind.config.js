8cdb0d01dc98397ddd8e15b84ef330d2945035ef
mohitgoyat525 <montyits71@gmail.com>
2025-01-15 11:23:27 +0530

    add-font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pinkyUnicorn: ["pinky-unicorn", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
