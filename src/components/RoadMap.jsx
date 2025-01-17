15e8d39aa7a2cf07083e0c3a72f70a0d8b272c78
mohitgoyat525 <montyits71@gmail.com>
2025-01-17 14:01:18 +0530

    add-changes

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -4,16 +4,25 @@ import RoadmapImg from "../assets/images/png/roadmap-img.png";
 import RoadmapMobileImg from "../assets/images/webp/roadmap-mobile.webp";
 import SubHeading from "../common/SubHeading";
 import { ROADMAP_LIST } from "../utils/helper";
-import RightStars from "../assets/images/svg/roadmap-left-stars.svg";
 import LeftStars from "../assets/images/svg/roadmap-right-stars.svg";
+import TopRightStar from '../assets/images/png/roadmap-top-right-stars.png'
+import BottomLeftStar from '../assets/images/png/roadmap-bottom-left-stars.png'
 
 const RoadMap = () => {
   return (
-    <div id="roadmap" className="pt-[231px] pb-[240px] px-5 overflow-hidden max-2xl:pt-48 max-xl:pt-20 max-sm:pt-[60px] max-2xl:pb-52 max-xl:pb-20 relative">
+    <div
+      id="roadmap"
+      className="pt-[231px] pb-[240px] px-5 overflow-hidden max-2xl:pt-48 max-xl:pt-20 max-sm:pt-[60px] max-2xl:pb-52 max-xl:pb-20 relative"
+    >
       <img
-        className="absolute left-0 -top-10 max-2xl:max-w-96 max-xl:max-w-72 max-[767px]:max-w-[198px] max-[767px]:top-1/4 -z-[1] star-animation"
-        src={RightStars}
-        alt="start"
+        src={BottomLeftStar}
+        alt="stars"
+        className="absolute bottom-0 left-0 star-animation max-[767px]:max-w-[198px]"
+      />
+      <img
+        src={TopRightStar}
+        alt="stars"
+        className=" absolute top-0 right-0 star-animation max-[767px]:max-w-[198px]"
       />
       <img
         className="absolute right-0 bottom-0 max-2xl:max-w-60 max-xl:max-w-48 max-[767px]:max-w-[198px] max-[767px]:-bottom-10 -z-[1] star-animation"
@@ -40,7 +49,7 @@ const RoadMap = () => {
                 className={`h-[629px] max-2xl:mx-4 max-2xl:h-[580px] max-xl:h-[251px] pt-[127px] w-full max-w-[844px] max-2xl:max-w-[770px] max-xl:max-w-[650px] max-2xl:bg-contain bg-roadmap-clouds bg-no-repeat absolute flex justify-center items-center max-xl:pt-20 max-xl:justify-normal max-[575px]:justify-center ${
                   i === 0
                     ? "bg-roadmap-left-bg top-[6%] -left-9 max-2xl:-left-20 max-2xl:top-[8%] max-[767px]:left-0 max-xl:bg-roadmap-left-mobile max-xl:top-[13%] max-lg:left-12 max-xl:left-36"
-                    : i === 1 
+                    : i === 1
                     ? "bg-roadmap-right-bg top-[24%] right-9 max-2xl:-right-12 max-[767px]:right-0 max-xl:bg-roadmap-right-mobile max-[767px]:pt-8 max-xl:top-[33%] max-lg:-right-24 max-xl:-right-8"
                     : i === 2
                     ? "bg-roadmap-left-bg top-[43%] -left-6 max-2xl:-left-16 max-[767px]:left-0 max-xl:bg-roadmap-left-mobile max-xl:top-1/2 max-lg:left-12 max-xl:left-48"
